e89f8828e062d41ffa04bd3b2b32a65844edf588
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2025-01-08 03:05:51 +0530

    new

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
+const path = require("path");
 const app = express();
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 let newListItems = [];
 
-app.get("/", (req, res) => {
-    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    let today = new Date();
-    let day = today.toLocaleDateString("en-us", options);
-    res.render("list", { KindOfDay: day, newListItems: newListItems });
-});
+app.get("/",(req,res)=>{
 
-app.post("/", (req, res) => {
+    let options = {weekday:'long',year:'numeric',month:'long',day:'numeric'};
+    let today = new Date();
+    let day = today.toLocaleDateString("en-us",options);
+    res.render("list",{KindOfDay: day, newListItems: newListItems});
+})
+
+app.post("/",(req,res)=>{
     let newItem = req.body.newItem;
     newListItems.push(newItem);
-    res.redirect('/');
-});
+    res.redirect('/')
+})
 
-module.exports = app; // Export the app for Vercel
+app.listen(3000,()=>{
+    console.log("http://localhost:3000")
+})
