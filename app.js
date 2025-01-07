eec5b263ca80cfedbf53977325f7d5b584b2fa47
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2025-01-08 03:09:39 +0530

    new-com

--- app.js
+++ app.js
@@ -2,31 +2,31 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
+
 const app = express();
 app.set('view engine', 'ejs');
 
+
 app.set("views", path.join(__dirname, "views"));
-
-
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 let newListItems = [];
 
-app.get("/",(req,res)=>{
-
-    let options = {weekday:'long',year:'numeric',month:'long',day:'numeric'};
+app.get("/", (req, res) => {
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let today = new Date();
-    let day = today.toLocaleDateString("en-us",options);
-    res.render("list",{KindOfDay: day, newListItems: newListItems});
-})
+    let day = today.toLocaleDateString("en-us", options);
+    res.render("list", { KindOfDay: day, newListItems: newListItems });
+});
 
-app.post("/",(req,res)=>{
+app.post("/", (req, res) => {
     let newItem = req.body.newItem;
     newListItems.push(newItem);
-    res.redirect('/')
-})
+    res.redirect('/');
+});
 
-app.listen(3000,()=>{
-    console.log("http://localhost:3000")
-})
+
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+});
